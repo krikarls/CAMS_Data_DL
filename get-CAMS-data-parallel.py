ae8ec3c88d561ef4001d9a9aaa7f5a8571e06802
Alvaro Valdebenito <alvarov@met.no>
2021-12-01 16:19:42 +0100

    back to ProcessPoolExecutor

--- get-CAMS-data-parallel.py
+++ get-CAMS-data-parallel.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 from __future__ import annotations
 
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed
+
 from cdsapi import Client
 
-from .CAMS2_83 import date_range, download_data, ModelName
+from .CAMS2_83 import ModelName, date_range, download_data
+
 
 def main():
     client = Client()
     for date in date_range("2021-06-01", "2021-08-31"):
-        with ThreadPoolExecutor(max_workers=4) as executor:
+        with ProcessPoolExecutor(max_workers=4) as executor:
             futures = [
                 executor.submit(download_data, client, date, model)
                 for model in ModelName
